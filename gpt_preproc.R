88693b4fd53954e529e77237ecbdcd60bb52190e
Zach Studdiford <zstuddiford@gmail.com>
2024-02-12 12:17:21 -0600

    Add counterfactual in preproc
    - Add counterfactuals

--- gpt_preproc.R
+++ gpt_preproc.R
@@ -15,12 +15,12 @@ data_filtered <- data %>%
 # Pivot the data to a longer format, filter for causal scenarios, and create the `place` column
 long_data <- data_filtered %>%
   pivot_longer(cols = c(starts_with("scenario_1_causal_"), starts_with("scenario_2_causal_"), 
-                        starts_with("scenario_3_causal_")), 
+                        starts_with("scenario_3_causal_"), starts_with("Q429"), starts_with("Q43"), starts_with("Q44")), 
                names_to = "scenario", values_to = "value") %>%
   mutate(place = case_when(
-    scenario %in% c("scenario_3_causal_1", "scenario_3_causal_2", "scenario_3_causal_3") ~ place_3,
-    scenario %in% c("scenario_2_causal_1", "scenario_2_causal_2", "scenario_2_causal_3") ~ place_2,
-    scenario %in% c("scenario_1_causal_1", "scenario_1_causal_2", "scenario_1_causal_3") ~ place_1
+    scenario %in% c("scenario_3_causal_1", "scenario_3_causal_2", "scenario_3_causal_3", "Q429_3", "Q43_2", "Q44_3") ~ place_3,
+    scenario %in% c("scenario_2_causal_1", "scenario_2_causal_2", "scenario_2_causal_3", "Q429_2", "Q43_7", "Q44_2") ~ place_2,
+    scenario %in% c("scenario_1_causal_1", "scenario_1_causal_2", "scenario_1_causal_3", "Q429_1", "Q43_1", "Q44_1") ~ place_1,
   )) %>%
   select(place, scenario, value)  # Select only the place, scenario, and value columns
 
