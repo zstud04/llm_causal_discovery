95065e04d0f83489753059dfbdac666ee15b77d9
Zach Studdiford <zstuddiford@gmail.com>
2024-02-13 09:33:02 -0600

    Filtered data add rows
    - Add rows to filtered data

--- mturk_step2_filter.R
+++ mturk_step2_filter.R
@@ -1,7 +1,56 @@
 library(dplyr)
 
-# Define the row numbers to keep
-rows_to_keep <- c(6, 10, 11, 13, 17, 18, 20, 21, 22, 26, 27, 45, 56, 57, 58, 64, 65, 66, 83, 84, 85, 86, 87, 88, 89, 90, 91, 111, 110, 112, 113, 114, 115, 116, 117, 118, 128, 129, 130, 132, 134, 135, 136, 143)
+# TODO: define filtered questions in config- this is terrible code but no time right now
+rows_to_keep <- c(6,
+10,
+11,
+13,
+17,
+18,
+20,
+21,
+22,
+26,
+27,
+45,
+56,
+57,
+58,
+64,
+65,
+66,
+83,
+84,
+85,
+86,
+87,
+88,
+89,
+90,
+91,
+111,
+110,
+112,
+113,
+114,
+115,
+116,
+117,
+118,
+128,
+129,
+130,
+132,
+134,
+135,
+136,
+143,
+47,
+53,
+60,
+62,
+63,
+97)
 
 # Read the CSV file
 data <- read.csv("processed_mturkiter2.csv")
@@ -13,3 +62,5 @@ filtered_data <- data[rows_to_keep, ]
 write.csv(filtered_data, "filtered_data.csv", row.names = FALSE)
 
 print("Filtered data has been saved to filtered_data.csv")
+
+print(paste("The filtered data contains", nrow(filtered_data), "rows."))
